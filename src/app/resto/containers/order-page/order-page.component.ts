d01bb56215e168c0a658010adf79dce48cb40579
Hugo Regadas <dev.hugoregadas@gmail.com>
2022-02-24 11:31:59 +0100

    Form Group

--- src/app/resto/containers/order-page/order-page.component.ts
+++ src/app/resto/containers/order-page/order-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { RecipeGroupInterface, RecipeInterface } from '../../resto.interfaces';
 import { ApiService } from '../../services/api.service';
@@ -8,26 +9,40 @@ import { ApiService } from '../../services/api.service';
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit
+{
 
   datas!: RecipeGroupInterface[];
-  foods!: RecipeInterface;
+  recipes!: RecipeInterface;
+  form!: FormGroup;
 
 
   constructor(private services: ApiService) {
     this.getAllRecipes();
    }
 
-
-
- async ngOnInit(): Promise<void> {
-  }
- 
-async getAllRecipes(){
-  this.datas = await this.services.getRecipes();
+  async ngOnInit(): Promise<void> {
+    this.form = new FormGroup({
+      placeId: new FormControl(''),
+      recipes: new FormArray([])
+    });
     }
+  
+  addRecipes(recipe: RecipeInterface) {
+    const recipeForm = new FormGroup({
+      quantity: new FormControl(1),
+      recipe: new FormGroup({
+        title: new FormControl(recipe.title, Validators.required),
+        quantity: new FormControl(recipe.description, Validators.required),
+      })
+    });
+    (this.form.get('recipes') as FormArray).push(recipeForm);
+  } 
+  
 
 
+  async getAllRecipes(){
+    this.datas = await this.services.getRecipes();
+      }
+  
 }
-
-
